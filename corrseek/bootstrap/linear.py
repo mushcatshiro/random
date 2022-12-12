c135ffaec7850169d56b53063e3e8b833adf157a
shiro <yingjieho@hotmail.com>
2022-12-12 21:57:57 +0800

    rework to focus on model itself and adding mlinear

--- corrseek/bootstrap/linear.py
+++ corrseek/bootstrap/linear.py
@@ -1,45 +1,21 @@
-import matplotlib.pyplot as plt
+import pandas as pd
 from sklearn.linear_model import LinearRegression, HuberRegressor
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 from .base import BaseModel
 from utils import CHKPT
 
 
-class LinearModel(BaseModel):
+class linearModel(BaseModel):
     def __init__(
-        self, X, y, metadata, threshold: float=None, topn: int=None, chkpt: CHKPT=None
+        self, X, y, metadata, chkpt: CHKPT=None, run_name=None, report_dir=None
     ):
-        super().__init__()
-        """
-        TODO
-        to validate if single model object instantiation is enough
-        """
         self.X = X
         self.y = y
-        self.metadata = metadata
-        # self.mode = mode
-        self.topn = topn
-        self.threshold = threshold
-        self.model = self.determine_run()
-        self.chkpt = chkpt
-        # TODO figure out a more elagant way of handling modelname
-        self.modelname = None if not hasattr(metadata, "modelname") else metadata["modelname"]
+        self.metadata:dict = metadata
+        super().__init__(chkpt=chkpt, run_name=run_name, report_dir=report_dir)
 
-    def determine_run(self):
-        print(self.metadata["normality"])
-        if self.metadata["normality"] or self.metadata["force_normal"]:
-            return LinearRegression()
-        elif not self.metadata["normality"] or self.metadata["force_robust"]:
-            return HuberRegressor()
-        elif self.metadata["load_model"]:
-            self.modelname = self.metadata["load_model"]["modelname"]
-            self.model = self.chkpt.retrieve_model(self.modelname)
-        else:
-            raise ValueError(
-                "unable to determine run with the metadata provided"
-            )
-
-    def run(self, validate=False):
+    def run(self):
         reg = self.model.fit(self.X, self.y)
         self.report +=\
             "initial fitting:\n"\
@@ -50,48 +26,53 @@ class LinearModel(BaseModel):
             f"{self.metadata['X']}_{self.metadata['y']}_{self.model.__class__.__name__}"
         print(self.report)
         print(self.modelname)
-        # if self.topn:
-        #     idx = np.argpartition(abs(reg.coef_), -self.topn).reshape(-1)[:-self.topn]
-        #     drop_list = np.zeros(reg.coef_.shape)
-        #     np.put(drop_list, idx, 1)
-        #     drop_list = drop_list.astype(np.bool)
-        #     print(f"topn drop list: {drop_list}")
-        # elif self.threshold:
-        #     drop_list = (abs(reg.coef_) < self.threshold).reshape(-1)
-        #     print(f"th drop list: {drop_list}")
-        # if validate:
-        #     X = X.loc[:, drop_list]
-        #     reg2 = self.model.fit(X, y)
-        #     self.report +=\
-        #         "2nd fitting:\n"\
-        #         f"score: {reg2.score(X, y)}\n"\
-        #         f"coef: {reg2.coef_}\n"
-        #     if reg2.score(X, y) > reg.score(X, y):
-        #         self.report +=\
-        #             "2nd fitting score > initial fitting score, "\
-        #             "recommend to investigate"
         return self
 
-    def visualize(self, save=False):
-        pred = self.model.predict(self.X)
-        plt.scatter(self.X, self.y)
-        plt.plot(
-            self.X,
-            pred,
-            label=f"y={round(self.model.coef_[0], 2)}x+{round(self.model.intercept_, 2)}"
-        )
-        plt.plot(self.X, [0] * len(self.X), 'b')
-        plt.plot([0] * len(self.y), self.y, 'b')
-        plt.legend()
-        plt.show()
-        if save:
-            pass
-    
-    def save_model(self):
-        if self.modelname is not None:
-            self.chkpt.save_model(self.modelname, self.model)
-        else:
-            raise ValueError("modelname is not found")
 
-    def generate_report(self, dir):
-        pass
+class LinearModel(linearModel):
+    def determine_run(self):
+        if len(self.X.shape) > 1 or len(self.y.shape) > 1:
+            raise ValueError("expects 1D for X and y")
+        if self.metadata["normality"] or self.metadata["force_normal"]:
+            return LinearRegression()
+        elif not self.metadata["normality"] or self.metadata["force_robust"]:
+            return HuberRegressor()
+        elif self.metadata["load_model"]:
+            self.modelname = self.metadata["load_model"]["modelname"]
+            model = self.chkpt.retrieve_model(self.modelname)
+            if not model:
+                raise ValueError(
+                    f"model {self.modelname} "
+                    "does not exists in checkpoint database"
+                )
+            self.model = model
+        else:
+            return LinearRegression()
+
+
+class MLinearModel(linearModel):
+    def determine_run(self):
+        if not isinstance(self.X, pd.DataFrame):
+            pass
+        if len(self.X.shape) == 1 or len(self.y.shape) > 1:
+            raise ValueError("expects 2D for X and 1D for y")
+        if self.metadata["force_normal"]:
+            return LinearRegression()
+        elif self.metadata["force_robust"]:
+            return HuberRegressor()
+        elif self.metadata["load_model"]:
+            self.modelname = self.metadata["load_model"]["modelname"]
+            self.model = self.chkpt.retrieve_model(self.modelname)
+        else:
+            return LinearRegression()
+    
+    def run(self):
+        vif = pd.DataFrame()
+        vif["feature"] = self.X.columns
+        vif["VIF"] = [
+            variance_inflation_factor(self.X.values, i)
+            for i in range(len(self.X.columns))
+        ]
+        self.report += vif
+        self.report += "\n"
+        return super().run()
