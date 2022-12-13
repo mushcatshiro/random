8127da9314f6397d0f2ecf09d84202468ec2c3c1
shiro <yingjieho@hotmail.com>
2022-12-14 07:05:41 +0800

    update linear model testing

--- corrseek/tests/test_linear.py
+++ corrseek/tests/test_linear.py
@@ -6,33 +6,31 @@ from utils import DataGenerator, CHKPT
 from dataset_generator import dataset_generator
 
 
-REPORTDIR = os.path.join(os.getcwd(), 'testlinear')
-
-
 class TestLinearModel(unittest.TestCase):
     def setUp(self) -> None:
         self.dataset, _ = dataset_generator()
         self.datagenerator = DataGenerator(
             self.dataset,
             "col_0",
-            "datatime",
+            "datetime",
             "group",
             "nparray",
             "mp3s"
         )
-        os.mkdir(REPORTDIR)
-        if not os.path.exists(REPORTDIR):
-            raise FileNotFoundError
         self.db = CHKPT(os.getcwd(), "testdb")
+        self.cleanup_dir = None
         return super().setUp()
     
     def tearDown(self) -> None:
         # delete report
-        os.remove(REPORTDIR)
+        if self.cleanup_dir is not None:
+            import time
+            time.sleep(2)
+            os.rmdir(self.cleanup_dir)
         # delete db
         return super().tearDown()
     
-    def _test_default_model(self):
+    def test_default_model(self):
         for col in self.datagenerator.cols:
             X, y, _ = self.datagenerator.generate(col)
             m = self.datagenerator.metadata[col]
@@ -42,14 +40,15 @@ class TestLinearModel(unittest.TestCase):
                 X=X,
                 y=y,
                 metadata=m,
-                threshold=1,
-                chkpt=self.db
+                chkpt=self.db,
+                run_name="testlinear"
             )
             model.run()
-            model.generate_report(REPORTDIR)
+            # model.generate_report()
             model.save_model()
             self.modelname = model.modelname
             # self.assertTrue()
+        self.cleanup_dir = model.report_dir
     
     def test_force_model(self):
         pass
